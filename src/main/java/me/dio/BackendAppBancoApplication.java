c81ce01fb69ff3d5f81f13881ceecceca420af20
Pedro Mendonça <althander@gmail.com>
2024-08-30 12:23:35 -0300

    Bug fix de CORS

--- src/main/java/me/dio/BackendAppBancoApplication.java
+++ src/main/java/me/dio/BackendAppBancoApplication.java
@@ -1,8 +1,11 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "URL do server padrão")})
 @SpringBootApplication
 public class BackendAppBancoApplication {
 
