e5801664607064a274c47dc6b456844ed3341f1a
Pedro Mendonça <althander@gmail.com>
2024-08-30 10:42:22 -0300

    Criação do Service, Controller e adição de dependencia do swagger

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+
+            throw new IllegalArgumentException("Esse número de conta de usuário já existe");
+        }
+
+        return userRepository.save(userToCreate);
+    }
+}
