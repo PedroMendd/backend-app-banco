77c4ec60b9a4df45dee2628c8e0075e0b757e554
Pedro Mendonça <althander@gmail.com>
2024-08-30 11:41:37 -0300

    Atualizando Exception, criando perfil prd

---
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package me.dio.controller.exception;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final org.slf4j.Logger logger = (org.slf4j.Logger) LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException){
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException){
+        return new ResponseEntity<>("ID do recurso não encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
+        var message = "Aconteceu um problema inesperado, veja os logs";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
